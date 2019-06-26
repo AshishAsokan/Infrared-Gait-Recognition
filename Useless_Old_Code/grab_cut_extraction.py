7a21d06f0fcc2963a92336525e8cd58e0db7384f
AshishAsokan <raashish020@gmail.com>
2019-06-26 11:59:03 +0530

    Update grab_cut_extraction.py

--- Useless_Old_Code/grab_cut_extraction.py
+++ Useless_Old_Code/grab_cut_extraction.py
@@ -26,26 +26,23 @@ def calc_mean_background(path):
     return result
 
 
-def contour_validation(image):
+def contour_validation(image, size):
 
     contours, hierarchy = cv2.findContours(image, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-
     mask_contours = np.ones(image.shape[:2], np.uint8)
     for c in contours:
-        if cv2.contourArea(c) < 3000:
+        if cv2.contourArea(c) < size:
             cv2.drawContours(mask_contours, [c], -1, 0, -1)
 
     image = cv2.bitwise_and(image, image, mask=mask_contours)
-    image = cv2.erode(image, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)))
     return image
 
 
 def contour_closing(dilated_image, gradient_image):
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    ret, thresh = cv2.threshold(dilated_image, 20, 255, cv2.THRESH_BINARY)
+    thresh = cv2.adaptiveThreshold(dilated_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 11)
 
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(dilated_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     mask = np.zeros(gradient_image.shape[:2], np.uint8)
     height, width = gradient_image.shape[:2]
@@ -54,15 +51,12 @@ def contour_closing(dilated_image, gradient_image):
         cv2.drawContours(mask, [c], 0, (255, 255, 255), 1)
 
     mask1 = np.zeros((height + 2, width + 2), np.uint8)  # line 26
-    cv2.floodFill(mask, None, (0, 0), 255)  # line 27
+    cv2.floodFill(mask, mask1, (0, 0), 255)  # line 27
     mask_inv = cv2.bitwise_not(mask)
 
-    kernel_erode = cv2.getStructuringElement(cv2.MORPH_CROSS, (6, 6))
-    final_result = cv2.erode(mask_inv, kernel_erode)
-
-    cv2.imshow("Final", final_result)
-    cv2.imshow("Thresh", thresh)
-    cv2.waitKey(30)
+    cv2.imshow("Final", mask_inv)
+    cv2.imshow("Magnitude", gradient_image)
+    cv2.waitKey(0)
 
 
 def detect_roi(path, background):
@@ -75,8 +69,8 @@ def detect_roi(path, background):
         if ret is False:
             break
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-        kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6, 6))
+        kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
         roi = cv2.absdiff(frame, background)
         # ret, thresh = cv2.threshold(roi, 10, 255, cv2.THRESH_BINARY)
         # canny = cv2.Canny(thresh, 100, 250)
@@ -90,25 +84,14 @@ def detect_roi(path, background):
         dyabs = cv2.convertScaleAbs(derivative_y)
         magnitude = cv2.addWeighted(dxabs, 1.5, dyabs, 1.5, 0)
 
-        magnitude[magnitude < 30] = 0
-        mag = cv2.cvtColor(magnitude, cv2.COLOR_BGR2GRAY)
-        mag = cv2.dilate(mag, kernel)
-        # mag = cv2.morphologyEx(mag, cv2.MORPH_OPEN, kernel1, iterations=2)
+        magnitude[magnitude < 32] = 0
+        gray = cv2.cvtColor(magnitude, cv2.COLOR_BGR2GRAY)
+        con = contour_validation(gray, 800)
 
-        # Calculating image gradients using Sobel derivative
-        derivative_x = cv2.Sobel(mag, cv2.CV_64F, 1, 0)
-        derivative_y = cv2.Sobel(mag, cv2.CV_64F, 0, 1)
-
-        # Calculating magnitude of image gradients
-        dxabs = cv2.convertScaleAbs(derivative_x)
-        dyabs = cv2.convertScaleAbs(derivative_y)
-        magnitude1 = cv2.addWeighted(dxabs, 1.5, dyabs, 1.5, 0)
-        #canny = cv2.Canny(magnitude1, 100, 250)
-
-        final = contour_validation(magnitude1)
-        # contour_closing(mag, magnitude)
-        cv2.imshow("Dilate", final)
-        cv2.waitKey(30)
+        contour_closing(con, magnitude)
+        # cv2.imshow("Magnitude", magnitude)
+        # cv2.imshow("Contours", gray)
+        # cv2.waitKey(0)
 
     video.release()
 
