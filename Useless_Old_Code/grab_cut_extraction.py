682d53b9e15db39d17705816f95a2cb7841f3091
AshishAsokan <raashish020@gmail.com>
2019-06-26 14:24:06 +0530

    Update grab_cut_extraction.py

--- Useless_Old_Code/grab_cut_extraction.py
+++ Useless_Old_Code/grab_cut_extraction.py
@@ -38,11 +38,32 @@ def contour_validation(image, size):
     return image
 
 
+def skeleton(image):
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    skel = np.zeros(image.shape[:2], np.uint8)
+    dim = np.size(image)
+    done = False
+
+    while not done:
+        eroded = cv2.erode(image, kernel)
+        dilated = cv2.dilate(eroded, kernel)
+        temp = cv2.subtract(image, dilated)
+        skel = cv2.bitwise_or(skel, temp)
+        image = eroded.copy()
+
+        size = dim - cv2.countNonZero(image)
+        if size == dim:
+            done = True
+
+    return skel
+
+
 def contour_closing(dilated_image, gradient_image):
 
-    thresh = cv2.adaptiveThreshold(dilated_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 11)
+    thresh = cv2.adaptiveThreshold(dilated_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 3, 1)
 
-    contours, hierarchy = cv2.findContours(dilated_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     mask = np.zeros(gradient_image.shape[:2], np.uint8)
     height, width = gradient_image.shape[:2]
@@ -53,10 +74,11 @@ def contour_closing(dilated_image, gradient_image):
     mask1 = np.zeros((height + 2, width + 2), np.uint8)  # line 26
     cv2.floodFill(mask, mask1, (0, 0), 255)  # line 27
     mask_inv = cv2.bitwise_not(mask)
+    eroded = cv2.erode(mask_inv, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)))
 
-    cv2.imshow("Final", mask_inv)
+    cv2.imshow("Final", eroded)
     cv2.imshow("Magnitude", gradient_image)
-    cv2.waitKey(0)
+    cv2.waitKey(30)
 
 
 def detect_roi(path, background):
@@ -82,21 +104,18 @@ def detect_roi(path, background):
         # Calculating magnitude of image gradients
         dxabs = cv2.convertScaleAbs(derivative_x)
         dyabs = cv2.convertScaleAbs(derivative_y)
-        magnitude = cv2.addWeighted(dxabs, 1.5, dyabs, 1.5, 0)
+        magnitude = cv2.addWeighted(dxabs, 9.0, dyabs, 9.0, 3)
+        gray = cv2.cvtColor(magnitude, cv2.COLOR_BGR2GRAY)
 
         magnitude[magnitude < 32] = 0
-        gray = cv2.cvtColor(magnitude, cv2.COLOR_BGR2GRAY)
-        con = contour_validation(gray, 800)
 
-        contour_closing(con, magnitude)
-        # cv2.imshow("Magnitude", magnitude)
-        # cv2.imshow("Contours", gray)
-        # cv2.waitKey(0)
+        cv2.imshow("Magnitude", magnitude)
+        cv2.waitKey(30)
 
     video.release()
 
 
-path_video = r'E:\PES\CDSAML\DatasetC\videos\01010fn00.avi'
+path_video = r'E:\PES\CDSAML\DatasetC\videos\01001fn00.avi'
 back_image = calc_mean_background(path_video)
 detect_roi(path_video, back_image)
 cv2.destroyAllWindows()
